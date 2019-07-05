03190bda68a77065c1201dfaf1d233c7b059e54c
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2019-07-05 08:10:30 +0200

    refactored end_italic_handler and fixed bugs

--- app/handlers/end_italic_handler.py
+++ app/handlers/end_italic_handler.py
@@ -15,17 +15,24 @@ class EndItalicHandler(Handler):
     def handle(self, text_window: PaneControl,
                text: str, pass_count: int) -> Tuple[str, int]:
         text_window.SendKeys('{Left}', waitTime=0)
-        offset = 0
-        if text[pass_count-2] in (' ', '.', ',', '|', '\n'):
-            if text[pass_count-3] in '.':
-                pass
-            else:
-                text_window.SendKeys('{Shift}({Left})', waitTime=0)
-                self.it_invoke.Invoke(waitTime=0)
-                text_window.SendKeys('{Left}', waitTime=0)
-                offset += 1
+        offset = self.__set_offset(text, pass_count)
+        text_window.SendKeys(f'{{Shift}}({{Left {offset}}})', waitTime=0)
         self.it_invoke.Invoke(waitTime=0)
+        if offset > 0:
+            text_window.SendKeys('{Left}', waitTime=0)
+            self.it_invoke.Invoke(waitTime=0)
         text_window.SendKeys(f'€{{Right {offset}}}', waitTime=0)
         while self.it_access.State == 16:
             time.sleep(0.001)
-        return text, pass_count-1
+        return text, pass_count - 1
+
+    @staticmethod
+    def __set_offset(text: str, pass_count: int) -> int:
+        if text[pass_count - 3:pass_count - 1] in ('. ', ', '):
+            return 2
+        elif text[pass_count - 3:pass_count - 1] in ('..'):
+            return 0
+        elif text[pass_count - 2] in (' ', '.', ',', '|', '\n'):
+            return 1
+        else:
+            return 0
