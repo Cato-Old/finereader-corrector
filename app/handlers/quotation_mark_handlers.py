3c2720248a51434911f6dfc3aab3b97b0be0d033
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2019-07-22 21:06:04 +0200

    fixed bug with begin quotation mark on the beginning of line

--- app/handlers/quotation_mark_handlers.py
+++ app/handlers/quotation_mark_handlers.py
@@ -21,6 +21,9 @@ class BeginQuotationMarkHandler(Handler):
                text_pos: TextPosition) -> TextPosition:
         if text_pos[0:2] in (' "', '("', '>"'):
             text_window.SendKeys('{Right}{Shift}({Right})„', waitTime=0)
-            text_pos.text = text_pos.text.replace(text_pos[0], '„', 1)
+            text_pos.text = text_pos.text.replace(text_pos[1], '„', 1)
             text_pos += 2
+        elif text_pos[-2:0] in (' "', '("', '>"'):
+            text_window.SendKeys('{Shift}({Left})„', waitTime=0)
+            text_pos.text = text_pos.text.replace(text_pos[-2], '„', 1)
         return text_pos
